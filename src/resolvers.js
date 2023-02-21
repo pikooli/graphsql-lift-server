01acf6b0c9c7461a5b911ee896549f4ce8ea9877
pikooli <>
2023-02-21 20:56:25 +0100

    first push

---
+++ src/resolvers.js
@@ -0,0 +1,52 @@
+const fetch = require("node-fetch");
+const resolvers = {
+  Query: {
+    // get all tracks, will be used to populate the homepage grid of our web client
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome();
+    },
+    // this is make to test how slow it is to always fetch with normal fetch, because it do not have a cache
+    tracksForHomeFetch: async () => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
+    // get a single track by ID, for the track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
+    },
+  },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
+    },
+  },
+  Track: {
+    author: async ({ authorId }, _, { dataSources }) => {
+      // const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      // const res = await fetch(`${baseUrl}/author/${authorId}`);
+      // return res.json();
+      return dataSources.trackAPI.getAuthor(authorId);
+    },
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
+  },
+};
+
+module.exports = resolvers;
